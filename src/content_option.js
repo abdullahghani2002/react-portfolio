1d08a4c7957e96823f542bea628beadebf8d0503
a1b2d3357 <abdullahghani581@gmail.com>
2024-06-29 16:20:26 +0500

    personalized a but

--- src/content_option.js
+++ src/content_option.js
@@ -1,15 +1,15 @@
-const logotext = "JOHN";
+const logotext = "Abdullah";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Abdullah",
+    description: "I’m Abdullah _ a 4th year student Majoring in CS @ LUMS",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Abdullah Ghani",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
-        third: "I develop mobile apps",
+        third: "I like exploring new things",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
