e8f95df8ee7720de5083457b55793f875ca48c05
a1b2d3357 <abdullahghani581@gmail.com>
2025-04-09 02:42:59 +0500

    bruh

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -76,6 +76,8 @@ export const ContactUs = () => {
     // Send event to Meta Pixel when user types in the name field
     if (name === "name" && window.fbq) {
       window.fbq('trackCustom', 'UserNameInput', { userName: value,date_of_birth: value})
+      window.fbq('trackCustom', 'Date of Birth', {dob: value})
+
       // window.fbq('track','Subscribe');
       // window.fbq('track', 'Purchase', {currency: "USD", value: 30.00});
     }
